ce886c6d9c2e43d878e45a051dd5dca777fbdb5c
Ezra Ramatong <128799204+ezra-ramatong@users.noreply.github.com>
2024-10-24 16:26:22 +0200

    update nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,7 +2,7 @@ for _, source in ipairs({
 	"core.options",
 	"core.keymaps",
 	"core.lazy",
-	--"core.autocommands",
+	"core.autocommands",
 }) do
 	local status_ok, fault = pcall(require, source)
 	if not status_ok then
