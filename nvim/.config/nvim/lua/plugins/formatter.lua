10dce8b826ae9d6bdf1e86db3f9d2de481d34e72
Ezra Ramatong <128799204+ezra-ramatong@users.noreply.github.com>
2024-11-29 13:22:03 +0200

    update sway

--- nvim/.config/nvim/lua/plugins/formatter.lua
+++ nvim/.config/nvim/lua/plugins/formatter.lua
@@ -1,31 +1,32 @@
-return	{
-  "stevearc/conform.nvim",
-  lazy = false,
-  keys = {
-    {
-      "<leader>fe",
-      function()
-        require("conform").format({ async = true, lsp_fallback = true })
-      end,
-      mode = "",
-      desc = "[F]ormat buffer",
-    },
-  },
-  opts = {
-    notify_on_error = false,
-    format_on_save = function(bufnr)
-      local disable_filetypes = { c = true, cpp = true }
-      return {
-        timeout_ms = 500,
-        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-      }
-    end,
-    formatters_by_ft = {
-      javascript = { "prettierd" },
-      lua = { "stylua" },
-      markdown = { "prettierd" },
-      go = { "gofumt" },
-    },
-  },
+return {
+	"stevearc/conform.nvim",
+	lazy = false,
+	keys = {
+		{
+			"<leader>fe",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = "",
+			desc = "[F]ormat buffer",
+		},
+	},
+	opts = {
+		notify_on_error = false,
+		format_on_save = function(bufnr)
+			local disable_filetypes = { c = true, cpp = true }
+			return {
+				timeout_ms = 500,
+				lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+			}
+		end,
+		formatters_by_ft = {
+			javascript = { "prettierd" },
+			lua = { "stylua" },
+			markdown = { "prettierd" },
+			go = { "gofumt" },
+			c = { "clang-format" },
+			cpp = { "clang-format" },
+		},
+	},
 }
-
